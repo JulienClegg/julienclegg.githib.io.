2dba6cddffaf378505a5407b8c79c463fd4601b7
JulienClegg <89506070+JulienClegg@users.noreply.github.com>
2021-08-25 16:05:50 +1000

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,20 @@
+function openMenu() {
+
+    var x = document.getElementById("portfolioTopnav");
+
+    if (x.className === "topnav") {
+
+      x.className += " responsive";
+
+    } else {
+
+      x.className = "topnav";
+
+    }
+
+  }
+
+  function submission() {
+
+    alert('sick');
+  }
